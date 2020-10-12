673da45a55dcc144882bb1228addc26f28e42572
polishchlieb <biczu.yt@gmail.com>
2020-10-12 08:18:51 +0200

    handling unknown modules

--- evaluator/moduleLoader.hpp
+++ evaluator/moduleLoader.hpp
@@ -3,9 +3,13 @@
 
 #include <dlfcn.h>
 #include "Scope.hpp"
+#include "../tokenizer/FileReadError.hpp"
 
 auto loadModule(const char* path) -> soviet::Scope* {
     auto handle = dlopen(path, RTLD_LAZY);
+    if (!handle)
+        throw soviet::FileReadError("Unknown module");
+
     auto create = reinterpret_cast<soviet::Scope* (*)()>(
         dlsym(handle, "createObject")
     );
